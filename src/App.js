59ff3bd6015e955ebb9e28cead79a0addcf0d0af
hbadshah <honeybadsah09@gmail.com>
2024-02-27 15:47:15 +0530

    Add .gitignore file in src directory

---
+++ src/App.js
@@ -0,0 +1,13 @@
+ 
+import './App.css';
+import AgeCalculator from './component/AgeCalculator';
+ 
+function App() {
+  return (
+    <div className="App">
+      <AgeCalculator/>
+    </div>
+  );
+}
+ 
+export default App;
